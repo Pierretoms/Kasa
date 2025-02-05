8c5abc55d65aa90bd387b59fcc38021b394de3ef
Pierre toms <pierre.toms13@gmail.com>
2025-02-05 11:23:13 +0100

    animation dropdown sur le onclick de la flèche et switch de la flèche haute et basse en fonction de l'état false ou true

--- my-react-app/src/components/Main/DropDown.jsx
+++ my-react-app/src/components/Main/DropDown.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DropDown.scss';
 
 function DropDown({ titre, contenu }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className='appartements_description'>
       <div className="appartements_description_header">
         <h4>{titre}</h4>
-        <i className="fa-solid fa-angle-down"></i>
+        <i 
+          className={`fa-solid ${isOpen ? 'fa-angle-down' : 'fa-angle-up'}`} 
+          onClick={() => setIsOpen(!isOpen)}
+        ></i>
       </div>
-      <div className="appartements_description_contenu">
+      <div className={`appartements_description_contenu ${isOpen ? 'open' : ''}`}>
         {Array.isArray(contenu) ? (
           <ul>
             {contenu.map((item, index) => (
